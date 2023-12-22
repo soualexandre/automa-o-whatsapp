50d97d04fa18cb8c3582962948f6002902530133
Alexandre Souza <alexandreifto2@gmail.com>
2023-12-22 13:16:22 -0300

    fix_corrigindo_erro_caso_nao_exista_o_contato

--- pywhats.py
+++ pywhats.py
@@ -3,7 +3,22 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 import time
+# Função para aguardar a presença do elemento
+def wait_for_element_visible(by, value, timeout=3):
+    try:
+        time.sleep(2)
+        contato_element = driver.find_element("xpath", "//span[@class='p357zi0d r15c9g6i']".format(search_term))
+        if(contato_element.click()):
+            return True
+    except TimeoutException:
+        print(f"Elemento {value} não encontrado após {timeout} segundos.")
+        return False
 
 # Carregando o arquivo Excel
 tabela = pd.read_excel('contatos.xlsx', engine='openpyxl')
@@ -21,7 +36,6 @@ print("Aguarde enquanto o WhatsApp Web é carregado...")
 
 input("Pressione Enter após escanear o código QR no WhatsApp Web...")
 
-# Loop através dos contatos
 for index, contato in tabela.iterrows():
     search_box = driver.find_element("xpath", "//div[@class='g0rxnol2 ln8gz9je lexical-rich-text-input']")
 
@@ -31,59 +45,68 @@ for index, contato in tabela.iterrows():
     # Simule a digitação como se fosse feita pelo usuário
     ActionChains(driver).send_keys_to_element(search_box, search_term).perform()
 
-    # Aguarde alguns segundos para que a mensagem seja enviada
-    time.sleep(2)
-    ActionChains(driver).send_keys(Keys.ENTER).perform()
-    time.sleep(1)
+    try:
+        # Aguarde a visibilidade do elemento
+        if wait_for_element_visible(By.XPATH, "//div[@class='lhggkp7q ln8gz9je rx9719la']".format(search_term)):
+            print('Entrou aqui')
+        else:
+            try:
+                WebDriverWait(driver, 3).until(
+                EC.element_to_be_clickable((By.XPATH, "//div[@class='_13mgZ']"))
+            )
+            except TimeoutException as e:
+                print(f"Tempo de espera expirado: {e}")
 
-    # Adicione o caminho do arquivo de mídia desejado
-    media_path = contato["caminho_arquivo"]
+            media_path = contato["caminho_arquivo"]
 
-    if pd.isna(media_path):
-        send_box = driver.find_element("xpath", "//div[@class='to2l77zo gfz4du6o ag5g9lrv bze30y65 kao4egtt']")
+            if pd.isna(media_path):
+                send_box = driver.find_element("xpath", "//div[@class='to2l77zo gfz4du6o ag5g9lrv bze30y65 kao4egtt']")
 
-        # Mensagem para o contato
-        send_term = "Mensagem para {}".format(contato['nome'])
+                # Mensagem para o contato
+                send_term = contato["mensagem"]
 
-        ActionChains(driver).send_keys_to_element(send_box, send_term).perform()
+                ActionChains(driver).send_keys_to_element(send_box, send_term).perform()
 
-        time.sleep(2)
+                time.sleep(2)
 
-        # Pressione Enter para enviar a mensagem
-        ActionChains(driver).send_keys(Keys.ENTER).perform()
+                # Pressione Enter para enviar a mensagem
+                ActionChains(driver).send_keys(Keys.ENTER).perform()
 
-        # Aguarde um tempo antes de prosseguir para o próximo contato
-        time.sleep(2)
-    else:
-         # Anexar Mídia
-        time.sleep(1)
-        attach_button = driver.find_element("xpath", "//div[@title='Anexar']")
-        attach_button.click()
-        # Localize o botão de upload de arquivo
-        upload_input = driver.find_element("xpath", "//input[@accept='image/*,video/mp4,video/3gpp,video/quicktime']")
+            else:
+                # Anexar Mídia
+                time.sleep(1)
+                attach_button = driver.find_element("xpath", "//div[@title='Anexar']")
+                attach_button.click()
+                # Localize o botão de upload de arquivo
+                upload_input = driver.find_element("xpath", "//input[@accept='image/*,video/mp4,video/3gpp,video/quicktime']")
 
-        # Envie o caminho do arquivo
-        upload_input.send_keys(media_path)
+                # Envie o caminho do arquivo
+                upload_input.send_keys(media_path)
 
-        # Aguarde o upload da mídia
-        time.sleep(2)
+                # Aguarde o upload da mídia
+                time.sleep(2)
 
-        send_box = driver.find_element("xpath", "//div[@class='to2l77zo gfz4du6o ag5g9lrv fe5nidar kao4egtt']")
+                send_box = driver.find_element("xpath", "//div[@class='to2l77zo gfz4du6o ag5g9lrv fe5nidar kao4egtt']")
 
-        # Mensagem para o contato
-        send_term = "Mensagem para {}".format(contato['nome'])
+                # Mensagem para o contato
+                send_term = contato["mensagem"]
 
-        ActionChains(driver).send_keys_to_element(send_box, send_term).perform()
+                if not (pd.isna(send_term)):
+                    ActionChains(driver).send_keys_to_element(send_box, send_term).perform()
 
-        time.sleep(2)
+                time.sleep(2)
 
-        # Pressione Enter para enviar a mensagem
-        ActionChains(driver).send_keys(Keys.ENTER).perform()
+                # Pressione Enter para enviar a mensagem
+                ActionChains(driver).send_keys(Keys.ENTER).perform()
 
-        # Aguarde um tempo antes de prosseguir para o próximo contato
-        time.sleep(2)
+                # Aguarde um tempo antes de prosseguir para o próximo contato
+                time.sleep(2)
+            
+    except NoSuchElementException:
+        print("Contato {} não encontrado. Pulando para o próximo contato.".format(search_term))
+    finally:
+        # Limpe o campo de busca antes de prosseguir para o próximo contato
+        ActionChains(driver).send_keys_to_element(search_box, Keys.BACKSPACE * len(search_term)).perform()
 
-# Feche o navegador após enviar para todos os contatos
-time.sleep(2000000)
-    
+time.sleep(5)
 driver.quit()
